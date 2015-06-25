515e26431f3990f04c0e0f861b6d05fc28646329
Sam Musso <sam.musso90@gmail.com>
2015-06-25 19:24:13 -0400

    Renamed Point class to UberXYZ_RGB. Added rgb un/pack routines. Dependency on PCLVisualizer added.

--- src/customPCD.cpp
+++ src/customPCD.cpp
@@ -1,32 +1,41 @@
 #include <pcl/point_cloud.h>
+#include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/io/pcd_io.h>
 
-struct MyPointType
+#define BYTE 8
+/* Following Boilerplate largely copy pasta'd from pointclouds.org */
+struct UberXYZ_RGB
 {
   PCL_ADD_POINT4D;                  // preferred way of adding a XYZ+padding
   float rgb;
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW   // make sure our new allocators are aligned
 } EIGEN_ALIGN16;                    // enforce SSE padding for correct memory alignment
 
-POINT_CLOUD_REGISTER_POINT_STRUCT (MyPointType,           // here we assume a XYZ + "test" (as fields)
+POINT_CLOUD_REGISTER_POINT_STRUCT (UberXYZ_RGB,
                                    (float, x, x)
                                    (float, y, y)
                                    (float, z, z)
                                    (float, rgb, rgb)
 )
 
-// pack r/g/b into rgb 
-void pack_rgb(MyPointType p){
-	uint8_t r = 255, g = 0, b = 0;    // Example: Red color 
-	uint32_t rgb = ((uint32_t)r << 16 | (uint32_t)g << 8 | (uint32_t)b); 
-	p.rgb = *reinterpret_cast<float*>(&rgb); 
+// Pack bytes into float (allegedly helps with SSE performance)
+void pack_rgb(UberXYZ_RGB *p, uint8_t rgb[3]){
+	uint32_t rgb_mask = ((uint32_t)rgb[0] << 16 | (uint32_t)rgb[1] << 8 | (uint32_t)rgb[2]);
+	p->rgb = *reinterpret_cast<float*>(&rgb_mask); // Type coursion int -> float
 }
 
+// Unpack routine TODO: break these into utils file
+void unpack_rgb(UberXYZ_RGB *p, uint8_t *rgb[3]){
+  uint32_t rgb_mask = *reinterpret_cast<uint32_t*>(&p->rgb);
+  *rgb[0] = (uint8_t)(rgb_mask >> 2 * BYTE);
+  *rgb[1] = (uint8_t)(rgb_mask >> BYTE);
+  *rgb[2] = (uint8_t)(rgb_mask);
+}
 
 int
 main (int argc, char** argv)
 {
-  pcl::PointCloud<MyPointType> cloud;
+  pcl::PointCloud<UberXYZ_RGB> cloud;
   cloud.points.resize (2);
   cloud.width = 2;
   cloud.height = 1;
