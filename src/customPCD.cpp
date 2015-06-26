2cd31948d220f02c676f8dba5c2f8d99ab1d768f
Sam Musso <sam.musso90@gmail.com>
2015-06-25 20:00:39 -0400

    Tested un/pack routines: PASS. Lets get these points drawn!

--- src/customPCD.cpp
+++ src/customPCD.cpp
@@ -25,25 +25,50 @@ void pack_rgb(UberXYZ_RGB *p, uint8_t rgb[3]){
 }
 
 // Unpack routine TODO: break these into utils file
-void unpack_rgb(UberXYZ_RGB *p, uint8_t *rgb[3]){
+void unpack_rgb(UberXYZ_RGB *p, uint8_t (&rgb)[3]){
   uint32_t rgb_mask = *reinterpret_cast<uint32_t*>(&p->rgb);
-  *rgb[0] = (uint8_t)(rgb_mask >> 2 * BYTE);
-  *rgb[1] = (uint8_t)(rgb_mask >> BYTE);
-  *rgb[2] = (uint8_t)(rgb_mask);
+  rgb[0] = (uint8_t)(rgb_mask >> 2 * BYTE);
+  rgb[1] = (uint8_t)(rgb_mask >> BYTE);
+  rgb[2] = (uint8_t)(rgb_mask);
 }
 
 int
 main (int argc, char** argv)
 {
   pcl::PointCloud<UberXYZ_RGB> cloud;
-  cloud.points.resize (2);
-  cloud.width = 2;
+  cloud.points.resize (3);
+  cloud.width = 3;
   cloud.height = 1;
 
-  cloud.points[0].rgb = 1;
-  cloud.points[1].rgb = 2;
-  cloud.points[0].x = cloud.points[0].y = cloud.points[0].z = 0;
-  cloud.points[1].x = cloud.points[1].y = cloud.points[1].z = 3;
+  uint8_t red[3] = {255, 0, 0};
+  uint8_t green[3] = {0, 255, 0};
+  uint8_t blue[3] = {0, 0, 255};
 
-  pcl::io::savePCDFile ("test.pcd", cloud);
+// Lets test out our utils
+  pack_rgb(&cloud.points[0] , red);
+  pack_rgb(&cloud.points[1] , green);
+  pack_rgb(&cloud.points[2] , blue);
+
+  cloud.points[0].x = cloud.points[0].y = cloud.points[0].z = 0;
+  cloud.points[1].x = cloud.points[1].y = cloud.points[1].z = 1;
+  cloud.points[2].x = cloud.points[2].y = cloud.points[2].z = 2;
+
+// Save the file
+  pcl::io::savePCDFile ("pack_unpack.pcd", cloud);
+
+// Clear cloud
+  for(int pt=0; pt < cloud.size(); pt++)
+    cloud.points[pt] = {0, 0, 0, 0};
+
+// Load it back from disk
+  pcl::io::loadPCDFile ("pack_unpack.pcd", cloud);
+
+  for(int pt = 0; pt < 3; pt++){
+    uint8_t rgb[3] = {0,0,0};
+    unpack_rgb(&cloud.points[pt], rgb); // populate colors
+    printf("Point %d: {%f, %f, %f} RGB:%f\tR| %d\tG| %d\t B| %d\n", pt,
+      cloud.points[pt].x, cloud.points[pt].y, cloud.points[pt].z, cloud.points[pt].rgb,
+      rgb[0], rgb[1], rgb[2]);
+  }
+
 }
