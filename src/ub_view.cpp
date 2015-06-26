8c51d3f187804c2609575dda70b032ceea400038
Sam Musso <sam.musso90@gmail.com>
2015-06-26 12:50:48 -0400

    Succesfully added GUI control from OpenCV.

--- src/ub_view.cpp
+++ src/ub_view.cpp
@@ -1,18 +1,39 @@
 #include "ub_view.h"
 
+const int alpha_slider_max = 100;
+int alpha_slider;
+double alpha, beta;
+
+cv::Mat src1, src2, dst;
+
+void on_trackbar( int as, void* p){
+  alpha = (double) alpha_slider / alpha_slider_max;
+  beta = (1.0 - alpha);
+
+  cv::addWeighted( src1, alpha, src2, beta, 0.0, dst );
+
+  cv::imshow( "Ub-GUI", dst);
+  int key = cv::waitKey(50);
+
+  if(key == 27)
+    exit(EXIT_SUCCESS);
+}
+
 int
 main (int argc, char** argv)
 {
   //pack_unpack_test();
   //binary_pcd_test();
 
+  src1 = cv::imread("../viz.jpg");
+  src2 = cv::imread("../rocket.jpg");
+
   pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud;
   pcl::PointCloud<pcl::PointXYZRGB> cloud_mem;
 
   pcl::io::loadPCDFile ("../model.pcd", cloud_mem);
-  uint8_t clr[3] = {255, 0, 0};
-  for(int pt = 0; pt < cloud_mem.size(); pt++)
-  {
+  uint8_t clr[3] = {0, 240, 0};
+  for(int pt = 0; pt < cloud_mem.size(); pt++){
     pack_rgb( &cloud_mem.points[pt], clr);
   }
 
@@ -28,7 +49,17 @@ main (int argc, char** argv)
   viewer->initCameraParameters();
   viewer->setCameraPosition(0.0, 0.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
+  printf("Creating window");
+  cv::namedWindow("Ub-GUI", 1);
+
+  char TrackbarName[50];
+  sprintf( TrackbarName, "Alpha : %d", alpha_slider);
+  cv::createTrackbar("Hello Buttons", "Ub-GUI", &alpha_slider, alpha_slider_max, &on_trackbar);
+
+
+
   while (!viewer->wasStopped ()){
+    on_trackbar( alpha_slider, 0);
     viewer->spinOnce(100);
     boost::this_thread::sleep (boost::posix_time::microseconds ( 100000));
   }
