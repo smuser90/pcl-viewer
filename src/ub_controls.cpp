baaa5450dc4dfafb07e7e0f8b9d0df64533008c6
Sam Musso <sam.musso90@gmail.com>
2015-06-26 22:36:18 -0400

    Changed Z/X keys to translate along up cam axis. Conflicts with pcl key bindings. Changing those next.

--- src/ub_controls.cpp
+++ src/ub_controls.cpp
@@ -118,18 +118,61 @@ void keyboard_handler(const pcl::visualization::KeyboardEvent &event, void* pvie
   }
 
   if (event.getKeySym() == "z" && event.keyDown()){
-    viewer->getCameraParameters (ub_camera);
+    viewer->getCameraParameters (ub_camera); // Keep camera fresh
 
-    viewer->setCameraPosition( ub_camera.pos[0], ub_camera.pos[1] + STEP, ub_camera.pos[2],
-      ub_camera.focal[0], ub_camera.focal[1], ub_camera.focal[2],
-      ub_camera.view[0], ub_camera.view[1], ub_camera.view[2]);
+    double *xyz = ub_camera.pos;
+    double *lookat = ub_camera.view;
+    double res[3];
+
+    for( int idx = 0; idx < 3; idx++)
+      res[idx] = lookat[idx] - xyz[idx];
+
+    float x = res[0];
+    float y = res[1];
+    float z = res[2];
+
+    reduce_to_unit(x, y, z);
+
+    ub_movement(0,0) = x * STEP;
+    ub_movement(0,1) = y * STEP;
+    ub_movement(0,2) = z * STEP;
+
+    viewer->setCameraPosition(ub_movement(0,0) + ub_camera.pos[0],
+        ub_movement(0,1) + ub_camera.pos[1],
+        ub_movement(0,2) + ub_camera.pos[2],
+        ub_camera.focal[0] + ub_movement(0,0),
+        ub_movement(0,1) + ub_camera.focal[1],
+        ub_movement(0,2) + ub_camera.focal[2],
+        ub_camera.view[0], ub_camera.view[1], ub_camera.view[2]);
   }
 
-  if ( event.isCtrlPressed() && event.keyDown()){
-    viewer->getCameraParameters (ub_camera);
-    viewer->setCameraPosition( ub_camera.pos[0], ub_camera.pos[1] - STEP, ub_camera.pos[2], 
-      ub_camera.focal[0], ub_camera.focal[1], ub_camera.focal[2],
-      ub_camera.view[0], ub_camera.view[1], ub_camera.view[2]);
+  if (event.getKeySym() == "x" && event.keyDown()){
+    viewer->getCameraParameters (ub_camera); // Keep camera fresh
+
+    double *xyz = ub_camera.pos;
+    double *lookat = ub_camera.view;
+    double res[3];
+
+    for( int idx = 0; idx < 3; idx++)
+      res[idx] = xyz[idx] - lookat[idx]; // - xyz[idx];
+
+    float x = res[0];
+    float y = res[1];
+    float z = res[2];
+
+    reduce_to_unit(x, y, z);
+
+    ub_movement(0,0) = x * STEP;
+    ub_movement(0,1) = y * STEP;
+    ub_movement(0,2) = z * STEP;
+
+    viewer->setCameraPosition(ub_movement(0,0) + ub_camera.pos[0],
+        ub_movement(0,1) + ub_camera.pos[1],
+        ub_movement(0,2) + ub_camera.pos[2],
+        ub_camera.focal[0] + ub_movement(0,0),
+        ub_movement(0,1) + ub_camera.focal[1],
+        ub_movement(0,2) + ub_camera.focal[2],
+        ub_camera.view[0], ub_camera.view[1], ub_camera.view[2]);
 
   }
 
